f6c9f80f392b6783f1006044a5cb133fa92f5171
Mingmingkhan <tasnuva.ming.khan@fau.de>
2020-04-23 21:58:46 +0200

    Subset Germany Covid-19 data by beginning from March 1, 2020 till date Plotted prediction models
    NOTE TO SELF: Consider writing functions

--- 01-time-series-covid19.R
+++ 01-time-series-covid19.R
@@ -17,9 +17,9 @@ dea <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/maste
 
 # Data for Germany (All dates)
 
-infe <- inf[,-c(1:4)]
-dead <- dea[,-c(1:4)]
-reco <- rec[,-c(1:4)]
+infe <- inf[,-c(1:4)] #infection
+dead <- dea[,-c(1:4)] #death
+reco <- rec[,-c(1:4)] #fatality
 i.country <- aggregate(infe, by=list(inf$Country.Region), FUN=sum)
 r.country <- aggregate(reco, by=list(rec$Country.Region), FUN=sum)
 d.country <- aggregate(dead, by=list(dea$Country.Region), FUN=sum)
@@ -29,17 +29,17 @@ d <- d.country[66,-1]
 
 # which(d.country$Group.1=="Germany")
 
-i <- as.numeric(i.country[66,-1])
-r <- as.numeric(r.country[66,-1])
-d <- as.numeric(d.country[66,-1])
+iN <- as.numeric(i.country[66,-1]) #adding N for "as.numeric" to make my life easier later
+rN <- as.numeric(r.country[66,-1])
+dN <- as.numeric(d.country[66,-1])
 inds <- seq(as.Date("2020-01-22"), (Sys.Date()-1), by = "day")
 
 # When first infection reported?
-st <- which(i>0)[1]
+st <- which(iN>0)[1]
 inds <- inds[-(1:st)]
-i <- i[-(1:st)]
-r <- r[-(1:st)]
-d <- d[-(1:st)]
+i <- iN[-(1:st)]
+r <- rN[-(1:st)]
+d <- dN[-(1:st)]
 
 quartz(w = 8, h = 5)
 op <- par(mfrow=c(1,2))
@@ -57,6 +57,9 @@ dd <- diff(d)
 
 d.inds <- inds[-1]
 
+## PLOTTING INFECTIONS, FITTING MODELS
+
+quartz()
 plot(d.inds, di, col="red", type="l", xlab="Date", ylab = "Number of cases", 
      main = "Daily Rate of Increase")
 lines(d.inds, dr, col="green")
@@ -75,10 +78,13 @@ legend("topleft",legend = c("Infections", "Recoveries","Fatalities"),
 
 # Spring forecast  
 
+## PLOTTING FORECASTS 
+
 # forecast for infection rates (classical gam extrapolation)
 
 quartz(w = 8, h = 5)
 op <- par(mfrow=c(1,2))
+
 fore <- 100
 d.pre <- predict(fit.gi, data.frame(x=seq(1,fore)), link="log", type="response")
 inds <- seq(as.Date(d.inds[1]), as.Date(d.inds[1])+fore-1, by=1)
@@ -95,5 +101,54 @@ plot(fo) # Why cannot I add axes labels??
 
 # Case 1: Start date from March 1, 2020 
 
-inds0103 <- seq(as.Date("2020-01-22"), (Sys.Date()-1), by = "day")
+startD <- which(date=="X3.1.20") #to find col number of March 1, 2020
+inf0103 <- iN[startD:length(iN)] #infection rate per day since March 1, 2020
+rec0103 <- rN[startD:length(iN)] #recovery rate
+dea0103 <- dN[startD:length(iN)] #death rate
+inds0103 <- seq(as.Date("2020-03-01"), (Sys.Date()-1), by = "day")
+
+# First differences and gam smoothing
+di0103 <- diff(inf0103)
+dr0103 <- diff(rec0103)
+dd0103 <- diff(dea0103)
+
+d.inds0103 <- inds0103[-1]
+
+## PLOTTING INFECTIONS, FITTING MODELS
+
+quartz(w=7,h=5)
+plot(d.inds0103, di0103, col="red", type="l", xlab="Date", ylab = "Number of cases", 
+     main = "Daily Rate of Increase Since March 1, 2020", ylim=c(0,10000))
+lines(d.inds0103, dr0103, col="green")
+lines(d.inds0103, dd0103, col="brown")
+
+y <- c(1:length(d.inds0103))
+fit.gi0103 <- gam(di0103~s(y))
+fit.gr0103 <- gam(dr0103~s(y))
+fit.gd0103 <- gam(dd0103~s(y))
+lines(d.inds0103, predict(fit.gi0103), col="red")
+lines(d.inds0103, predict(fit.gr0103), col="green")
+lines(d.inds0103, predict(fit.gd0103), col="brown")
+legend("topleft",legend = c("Infections", "Recoveries","Fatalities"), 
+       col= c("red", "green", "brown"), lty = 1)
+
+## PLOTTING FORECASTS SINCE MARCH 1, 2020
+
+# forecast for infection rates (classical gam extrapolation)
+
+quartz(w = 8, h = 5)
+op <- par(mfrow=c(1,2))
+
+fore <- 100
+d.pre0103 <- predict(fit.gi0103, data.frame(y=seq(1,fore)), link="log", type="response")
+inds0103 <- seq(as.Date(d.inds0103[1]), as.Date(d.inds0103[1])+fore-1, by=1)
+
+plot(inds0103, d.pre0103, type="l", xlab = "Date", ylab = "Number of cases", 
+     main = "GAM Infection: start 01.03.2020")
+
+# forecast for infection rates (Arima)
+fit0103 <- auto.arima(di0103)
+fo0103 <- forecast(fit0103,h=20)
+plot(fo0103) # Why cannot I add axes labels?? 
+
 
